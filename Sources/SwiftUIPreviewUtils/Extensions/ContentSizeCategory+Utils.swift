9d81c9b5913e67c3a3916005bf0eba946e88b7ac
CypherPoet <CypherPoet@gmail.com>
2021-03-12 01:30:01 -0600

    Remove public ContentSizeCategory.previewName extension

--- Sources/SwiftUIPreviewUtils/Extensions/ContentSizeCategory+Utils.swift
+++ Sources/SwiftUIPreviewUtils/Extensions/ContentSizeCategory+Utils.swift
@@ -5,19 +5,6 @@ import SwiftUI
 
 extension ContentSizeCategory {
     public static let smallestAndLargest = [allCases.first!, allCases.last!]
-
-
-    public var previewName: String {
-        if self == Self.smallestAndLargest.first {
-            return "Smallest Content Size"
-        }
-
-        if self == Self.smallestAndLargest.last {
-            return "Largest Content Size"
-        }
-
-        return String(describing: self)
-    }
 }
 
 #endif
